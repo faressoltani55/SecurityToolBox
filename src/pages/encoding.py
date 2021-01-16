4e05eaee83ecf42a202404655da15c895da2bfeb
HaninAyadi <Ayadi.Hanin@insat.u-carthage.tn>
2021-01-16 20:02:39 +0100

    encoding update

--- src/pages/encoding.py
+++ src/pages/encoding.py
@@ -2,32 +2,30 @@ import utils.enc_functions as enc
 import streamlit as st
 
 
-def process_input(encoding, input, file=False):
+def process_input(encoding, text):
     right, left = st.beta_columns(2)
     with right:
         encode = st.button('Encode')
     with left:
         decode = st.button('Decode')
     if encode:
-        return enc.encode_text(encoding, input)
+        return enc.encode_text(encoding, text)
     elif decode:
-        return enc.decode_text(encoding, input)
+        return enc.decode_text(encoding, text)
+
 
 def write():
-    st.title("Encode/Decode text or file: ")
+    st.title("Encode/Decode text")
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-    input_type = st.radio("Pick input type: ", ["Text input", "File input"])
 
-    if input_type == "Text input":
-        input = st.text_area("Write your message here")
-    elif input_type == "File input":
-        input = st.file_uploader("Choose a file to process", accept_multiple_files=False)
+    input_text = st.text_area("Write your message here")
 
-    encoding_type = st.selectbox("Start by choosing the type of encoding to deal with: ", ["Standard text encoding", "Base64", "URL"])
+    encoding_type = st.selectbox("Start by choosing the type of encoding to deal with: ",
+                                 ["Standard text encoding", "Base64", "URL"])
 
     if encoding_type == "Standard text encoding":
         encoding = st.selectbox("Pick the encoding: ", enc.get_standard_text_encodings())
-        result = process_input(encoding, input)
+        result = process_input(encoding, input_text)
         st.text(result)
     elif encoding_type == "Base64":
         return
--- utils/enc_functions.py
+++ utils/enc_functions.py
@@ -1,7 +1,7 @@
 import requests
 import lxml.html
 import codecs
-
+import base64
 
 def get_standard_text_encodings():
     version, url = ('3.9', 'https://docs.python.org/3.9/library/codecs.html#standard-encodings')
@@ -12,25 +12,25 @@ def get_standard_text_encodings():
     return encodings
 
 
+def base64_encode(binary):
+    return base64.b64encode(binary)
+
+
+def base64_decode(text):
+    return base64.b64decode(text)
+
 def encode_text(encoding, text):
     """ convert unicoded strings into any encodings supported by the current version of Python """
 
-    encoded_text = text.encode(encoding, 'strict')
-
-    return encoded_text
+    #encoded_text = text.encode(encoding, 'strict')
+    encoded_text = codecs.encode(text, encoding)
+    return base64.b64encode(encoded_text)
 
 
 def decode_text(encoding, text):
     """ convert encoded strings (with any encodings supported by the current version of Python) to unicoded string """
 
-    decoded_text = bytes(text, encoding='utf-8').decode(encoding, errors="ignore")
+    #decoded_text = bytes(text, encoding='utf-8').decode(encoding, errors="ignore")
 
-    return decoded_text
-
-
-def encode_file(encoding, file):
-    return
-
-
-def decode_file(encoding, file):
-    return
+    #decoded_text = codecs.decode(base64_decode(text), encoding)
+    return base64.b64decode(text)
