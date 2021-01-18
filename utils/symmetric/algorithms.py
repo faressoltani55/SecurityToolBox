d1ad8139c35c7b2ef54c39eb2e04f62f7e62aa2d
HaninAyadi <Ayadi.Hanin@insat.u-carthage.tn>
2021-01-18 20:34:16 +0100

    Added algorithm propreties constants

--- utils/symmetric/algorithms.py
+++ utils/symmetric/algorithms.py
@@ -4,7 +4,24 @@ SYM_ALGORITHMS = [
     'CAST5',
     'SEED',
     'Camellia',
-    'ChaCha20',
     'Blowfish',
     'IDEA',
 ]
+
+def key_sizes_gen(min_size, max_size):
+    key_sizes = []
+    x = min_size
+    while x <= max_size:
+        key_sizes.append(x)
+        x = x+8
+    return key_sizes
+
+SYM_ALGORITHMS_PROPS = {
+    'AES': {'strength': 1, 'key_sizes': [128, 192, 256], 'block_size': 128},
+    'TripleDES': {'strength': 1, 'key_sizes': [64, 128, 192], 'block_size': 64},
+    'CAST5': {'strength': 1, 'key_sizes': key_sizes_gen(40, 148), 'block_size': 64},
+    'SEED': {'strength': 1, 'key_sizes': [128], 'block_size': 128},
+    'Camellia': {'strength': 1, 'key_sizes': [128, 192, 256], 'block_size': 128},
+    'Blowfish': {'strength': 0, 'key_sizes': key_sizes_gen(32, 448), 'block_size': 64},
+    'IDEA': {'strength': 0, 'key_sizes': [128], 'block_size': 64}
+}
